25183447f8ef6778f8336aee1ba7cca3feeb83d7
akramab <13519142.std.stei.itb.ac.id>
2021-04-17 12:43:18 +0700

    Create Skill.java

---
+++ Skill.java
@@ -0,0 +1,59 @@
+public class Skill {
+    private String skillName;
+    private int basePower;
+    private int masteryLevel;
+    private Vector<Element>elements;
+
+    public Skill() {
+        skillName = "Stand still";
+        basePower = 10;
+        masteryLevel = 1;
+        elements = new Vector<Element>();
+        elements.add(FIRE);
+        elements.add(WATER);
+        elements.add(ELECTRIC);
+        elements.add(GROUND);
+        elements.add(ICE);
+    }
+
+    public Skill(String sName, int bPower, int mLevel, String elmt) {
+        skillName = sName;
+        basePower = bPower;
+        masteryLevel = mLevel;
+        elements.add(elmt);
+    }
+
+    public Skill(String sName, int bPower, int mLevel, Vector<Element> elmts) {
+        skillName = sName;
+        basePower = bPower;
+        masteryLevel = mLevel;
+
+        for (int i = 0; i < elmts.size(); i++) {
+            elements.add(elmts[i]);
+        }
+    }
+
+    public String getSkillName() {
+        return skillName;
+    }
+
+    public int getBasePower(){
+        return basePower;
+    }
+
+    public int getMasteryLevel(){
+        return masteryLevel;
+    }
+
+    public Vector<Element> getElement() {
+        return elements;
+    }
+
+    public void masteryLevelUp() {
+        masteryLevel++;
+    }
+
+    public boolean isEqual(Skill s) {
+        return skillName == s.getSkillName();
+    }
+}
