1ebbd686bba7d1518975171f229c070db5ca4fad
LastAeon <53500064+LastAeon@users.noreply.github.com>
2021-04-24 13:02:28 +0700

    bikin inventory

---
+++ Inventory.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class Inventory<T> {
+    private ArrayList<T> list = new ArrayList<T>();
+
+    public Inventory(){}
+
+    public ArrayList<T> getArray(){
+        return list;
+    }
+
+    public void add(T t){
+        list.add(t);
+    }
+
+    public void removeAtIdx(int idx){
+        list.remove(idx);
+    }
+
+    public int getSize(){
+        return list.size();
+    }
+
+    public T getElement(int i){
+        return list.get(i);
+    }
+}
+//    private:
+//    vector<T> list;
+//
+//template <typename T>
+//Inventory<T>::Inventory(){}
+//
+//        template <typename T>
+//Inventory<T>::~Inventory(){}
+//
+//        template <typename T>
+//vector<T>& Inventory<T>::getVector(){
+//        return this->list;
+//        }
+//
+//        template <typename T>
+//void Inventory<T>::add(T t){
+//        this->list.push_back(t);
+//        }
+//
+//        template <typename T>
+//void Inventory<T>::removeAtIdx(int n){
+//        this->list.erase(this->list.begin()+n);
+//        }
+//
+//        template <typename T>
+//int Inventory<T>::getSize(){
+//        return this->list.size();
+//        }
+//
+//        template <typename T>
+//T Inventory<T>::getElement(int i){
+//        return list[i];
+//        }
+//
+//        }
